54875dfcd18f598b25594734ab4b45edbdf5a590
Jjav2 <Jjav2@HPCOMPUTER18>
2024-05-01 16:32:04 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Xml;
+
+namespace ReadXMLfromURL
+{
+   
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            String URLString = "https://www.w3schools.com/xml/cd_catalog.xml";
+            XmlTextReader reader = new XmlTextReader(URLString);
+
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element: 
+                        Console.Write("<" + reader.Name);
+
+                        while (reader.MoveToNextAttribute()) 
+                            Console.Write(" " + reader.Name + "='" + reader.Value + "'");
+ 
+                        Console.WriteLine(">");
+                        break;
+                    case XmlNodeType.Text: 
+                        Console.WriteLine(reader.Value);
+                        break;
+                    case XmlNodeType.EndElement: 
+                        Console.Write("</" + reader.Name);
+                        Console.WriteLine(">");
+                        break;
+                }
+            }
+        }
+    }
+}
